bfdbd35b551bd518e054f6249b3123b8e02afb10
ChuWeiChang <chuweichang0319@gmail.com>
2025-10-31 14:58:58 +0800

    feat: add initial control package and motor controller skeleton

---
+++ src/controller/motor_controller.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from src.core.events import EventBus, Event
+from src.core.logging import logger
+
+
+# class Controller:
+#     def __init__(self, bus: EventBus) -> None:
+#         self._bus = bus
+#
+#     async def __aenter__(self) -> "Controller":
+#         self._bus.subscribe("drive/set_velocity", self._apply_velocity)
+#         logger.info("MotorController started")
+#         return self
+#
+#     async def __aexit__(self, exc_type, exc, tb) -> None:
+#         logger.info("MotorController stopped")
+#
+#     # Hardware integration point
+#     def _apply_velocity(self, event: Event) -> None:
+#         # TODO: call JetBot motor APIs here (e.g., set_motor_speeds(left, right))
+#         if self._bus:
+#             pass
