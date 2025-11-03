7434380144373526f51ccdf3534c788dbe22f5ff
ChuWeiChang <chuweichang0319@gmail.com>
2025-11-03 10:43:07 +0800

    initial setup

--- src/controller/motor_controller.py
+++ src/controller/motor_controller.py
@@ -2,19 +2,23 @@ from __future__ import annotations
 
 from src.core.events import EventBus, Event
 from src.core.logging import logger
+from src.random_walk.random_walk import RandomWalkDaemon
+from src.safety.collision_avoidance import CollisionAvoidanceDaemon
+from src.perception.yolo_inference import YoloInference
 
 
-# class Controller:
-#     def __init__(self, bus: EventBus) -> None:
-#         self._bus = bus
-#
-#     async def __aenter__(self) -> "Controller":
-#         self._bus.subscribe("drive/set_velocity", self._apply_velocity)
-#         logger.info("MotorController started")
-#         return self
-#
-#     async def __aexit__(self, exc_type, exc, tb) -> None:
-#         logger.info("MotorController stopped")
+class Controller:
+    # Mix in the command from random walk, safety, YOLO, then publish to drive/set_velocity
+    def __init__(self, bus: EventBus, random_walk : RandomWalkDaemon, safety: CollisionAvoidanceDaemon, yolo: YoloInference) -> None:
+        self._bus = bus
+
+    async def __aenter__(self) -> "Controller":
+        # self._bus.subscribe("drive/set_velocity", self._apply_velocity)
+        logger.info("MotorController started")
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb) -> None:
+        logger.info("MotorController stopped")
 #
 #     # Hardware integration point
 #     def _apply_velocity(self, event: Event) -> None:
