cfe8c5d8ccbbe47c2a075296920d4b606feb7e0f
ChuWeiChang <chuweichang0319@gmail.com>
2025-11-03 10:35:50 +0800

    feat: update random walk logic and add command placeholder

--- src/behavior/random_walk.py
+++ src/behavior/random_walk.py
@@ -12,13 +12,11 @@ class RandomWalkDaemon(AbstractAsyncContextManager):
     """Moves the robot randomly until a stop signal is received."""
 
     def __init__(self, bus: EventBus) -> None:
+        self.command = None #TODO: command data structure 謙
         self._bus = bus
         self._task: Optional[asyncio.Task] = None
-        self._stopped = False
 
     async def __aenter__(self):
-        self._bus.subscribe("safety/stop", self._on_stop)
-        self._bus.subscribe("safety/clear", self._on_clear)
         self._task = asyncio.create_task(self._run())
         return self
 
@@ -30,22 +28,11 @@ class RandomWalkDaemon(AbstractAsyncContextManager):
             except asyncio.CancelledError:
                 pass
 
-    async def _on_stop(self, event: Event):
-        self._stopped = True
-        logger.warning(f"RandomWalk received stop: {event.payload}")
-
-    async def _on_clear(self, event: Event):
-        self._stopped = False
-        logger.warning(f"RandomWalk received clear: {event.payload}")
-
     async def _run(self):
         logger.info("RandomWalk started")
-        try:
-            while not self._stopped:
-                # TODO: send random drive commands to JetBot controller
-                logger.info("RandomWalk: step")
-                await asyncio.sleep(0.5)
-        finally:
-            # TODO: stop motors
-            logger.info("RandomWalk stopped")
+        while True:
+            # TODO: set random walk algo to self.command
+            self.command =None
+            logger.info("RandomWalk: step")
+            await asyncio.sleep(0.5)
 
