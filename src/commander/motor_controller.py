5ba59f2eeb1242f0adfc79845640e8a160ded8af
ChuWeiChang <chuweichang0319@gmail.com>
2025-12-27 22:44:28 +0800

    finished commander prototype

--- src/commander/motor_controller.py
+++ src/commander/motor_controller.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-
+import asyncio
 from src.core.events import EventBus, Event
 from src.core.logging import logger
 from src.random_walk.random_walk import RandomWalkDaemon
@@ -16,7 +16,7 @@ class Commander:
         self._yolo = yolo
 
     async def __aenter__(self) -> "Commander":
-        # self._bus.subscribe("drive/set_velocity", self._apply_velocity)
+        self._control_task = asyncio.create_task(self.apply_velocity())
         logger.info("MotorController started")
         return self
 
@@ -24,7 +24,18 @@ class Commander:
         logger.info("MotorController stopped")
 #
 #     # Hardware integration point
-#     def _apply_velocity(self, event: Event) -> None:
-#         # TODO: call JetBot motor APIs here (e.g., set_motor_speeds(left, right))
-#         if self._bus:
-#             pass
+    async def apply_velocity(self):
+        print("Robot Control: Online")
+        while True:
+
+            if not self._yolo.detected:
+                left = self._random_walk.command[0]
+                right = self._random_walk.command[1]
+            else:
+                left = self._yolo.command[0]
+                right = self._yolo.command[1]
+
+            payload = {"left": left, "right": right}
+            event = Event("drive/set_velocity", payload)
+            await self._bus.publish(event)
+            await asyncio.sleep(0.1)
